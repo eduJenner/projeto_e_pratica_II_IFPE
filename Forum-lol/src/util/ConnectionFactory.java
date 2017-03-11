15d1c7b570cadfbb5513d969d7f103558b245bad
dico93 <ed.jenner93@gmail.com>
2017-03-10 22:14:59 -0300

    commit

--- Forum-lol/src/util/ConnectionFactory.java
+++ Forum-lol/src/util/ConnectionFactory.java
@@ -10,7 +10,7 @@ public class ConnectionFactory {
 
 		String nomeBanco = "Forum";
 		String usuario = "root";
-		String senha = "";
+		String senha = "1";
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
